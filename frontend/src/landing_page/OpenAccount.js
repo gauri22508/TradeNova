cfca7517a30f1d441dfd996eb4b454a35e2d9f67
gaurisharma <gaurisharma9105@gmail.com>
2026-07-30 22:56:20 +0530

    supportPage

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -4,7 +4,7 @@ function OpenAccount() {
     return (
         <div className="container py-5 mb-5">
             <div className="row justify-content-center text-center">
-                <div className="col-lg-12 col-md-8 col-sm-4">
+                <div className=" col-md-8 col-sm-4">
                  
                     <h1 className="mt-3">Open a TradeNova account</h1>
                     <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
